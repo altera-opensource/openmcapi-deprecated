efb9dcf8136b6224990186f30cd8a206b46cd6c3
Hollis Blanchard <hollis_blanchard@mentor.com>
2011-05-26 14:27:42 -0700

    [test] Missed renaming one instance of MCAPI_BUF_COUNT
    Signed-off-by: Hollis Blanchard <hollis_blanchard@mentor.com>

--- test/func/fts_mcapi_pktchan_send_i.c
+++ test/func/fts_mcapi_pktchan_send_i.c
@@ -281,7 +281,7 @@ MCAPI_THREAD_ENTRY(MCAPI_FTS_Tx_2_17_3)
                 if (mcapi_struct->status == MCAPI_SUCCESS)
                 {
                     /* Exhaust all local buffers. */
-                    for (i = 0; i < MCAPI_BUF_COUNT; i ++)
+                    for (i = 0; i < TEST_BUF_COUNT; i ++)
                     {
                         /* Issue a NO OP to cause the other side to send an ACK. */
                         mcapi_struct->status =
